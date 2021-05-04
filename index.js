1087e56a711c189c764fd8142a624c39a43ac8cb
khamla@siyivang <ksiyivang@gmail.com>
2021-05-05 00:04:53 +0700

    variable connect nodejs container to mongodb container successfully

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ const express = require("express");
 const mongoose = require("mongoose")
 const app = express();
 
+const { MONGO_IP, MONGO_PASSWORD, MONGO_USERNAME, MONGO_PORT } = require("./config/config")
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    autoIndex: false, // Don't build indexes
-    poolSize: 10, // Maintain up to 10 socket connections
-    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    family: 4 // Use IPv4, skip trying IPv6
+    // autoIndex: false, // Don't build indexes
+    // poolSize: 10, // Maintain up to 10 socket connections
+    // serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    // socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    // family: 4 // Use IPv4, skip trying IPv6
 };
 
-mongoose.connect("mongodb://khamla:mypassword@mongo:27017/?authSource=admin", { useNewUrlParser: true })
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}?authSource=admin`
+
+mongoose.connect(url, options)
     .then(() => {
         console.log("successfully connect to mongdDB...at ")
     })
